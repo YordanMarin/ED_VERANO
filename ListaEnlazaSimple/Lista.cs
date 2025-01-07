23a662b160e635d7f36fb6472c0da99bb92afdac
YordanMarin <yordanmarinsovero@gmail.com>
2025-01-07 12:00:10 -0500

    Agregar archivos de proyecto.

---
+++ ListaEnlazaSimple/Lista.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaEnlazaSimple
+{
+    internal class Lista
+    {
+        private Nodo primero = null;
+        private Nodo ultimo = null;
+
+        public void insertar(int num)
+        {
+            Nodo nuevo = new Nodo();
+            nuevo.Numero = num;
+
+            if(primero == null)
+            {
+                primero = nuevo;
+                primero.Siguiente = null;
+                ultimo = nuevo;
+            }
+            else
+            {
+                ultimo.Siguiente = nuevo;
+                nuevo.Siguiente = null;
+                ultimo = nuevo;
+            }
+        }
+
+        public void mostrar(ListBox list)
+        {
+            Nodo actual = primero;
+
+            while(actual != null)
+            {
+                list.Items.Add(actual.Numero);
+                actual = actual.Siguiente;
+            }
+        }
+    }
+}
